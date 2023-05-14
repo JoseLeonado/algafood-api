b8cea4460d7c88634cb303707f55639fedc20dd4
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-05-14 20:20:27 -0300

    Implementando serviços REST de cidades e estados

--- src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
@@ -15,7 +17,7 @@ public class EstadoRepositoryImpl implements EstadoRepository {
 
 	@PersistenceContext
 	private EntityManager manager;
-
+	
 	@Override
 	public List<Estado> listar() {
 		return manager.createQuery("from Estado", Estado.class)
@@ -28,13 +30,20 @@ public class EstadoRepositoryImpl implements EstadoRepository {
 	}
 
 	@Override
+	@Transactional
 	public Estado salvar(Estado estado) {
 		return manager.merge(estado);
 	}
 
 	@Override
-	public void remover(Estado estado) {
-		estado = buscar(estado.getId());
+	@Transactional
+	public void remover(Long id) {
+		Estado estado = buscar(id);
+		
+		if (estado == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
 		manager.remove(estado);
 	}
 	

d4262e9eed8fc700a3f60a722528c294e537c2fc
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-05-31 16:04:52 -0300

    Preparando o projeto para testes de integração

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,11 +1,19 @@
 package com.algaworks.algafood;
 
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+public class AlgafoodApiApplicationTests {
 
-	void contextLoads() {
+	@Test
+	public void contextLoads() {
+		assertFalse(true);
 	}
 
 }

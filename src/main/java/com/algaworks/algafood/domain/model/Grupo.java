34c5264ed2b0878ab6ccc4302d79c4b4c8c209e4
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-06-17 15:48:13 -0300

    Implementando os endpoints de associação de usuários com grupos

--- src/main/java/com/algaworks/algafood/domain/model/Grupo.java
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -34,12 +34,12 @@ public class Grupo {
 			inverseJoinColumns = @JoinColumn(name = "permissao_id"))
 	private Set<Permissao> permissoes = new HashSet<>();
 	
-	public boolean adicionarPermissao(Permissao permissao) {
-		return getPermissoes().add(permissao);
-	}
-	
 	public boolean removerPermissao(Permissao permissao) {
 		return getPermissoes().remove(permissao);
+	}
+	
+	public boolean adicionarPermissao(Permissao permissao) {
+		return getPermissoes().add(permissao);
 	}
 	
 }

a1927af6ee5f5d8e3787d646d29e72c529ce075a
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-09-13 11:33:20 -0300

    Descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.GrupoModelAssembler;
 import com.algaworks.algafood.api.model.GrupoModel;
+import com.algaworks.algafood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.service.CadastroUsuarioService;
 
 @RestController
-@RequestMapping("/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = "/usuarios/{usuarioId}/grupos",
+		produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 	
 	@Autowired
 	private CadastroUsuarioService cadastroUsuario;
@@ -27,6 +30,7 @@ public class UsuarioGrupoController {
 	@Autowired
 	private GrupoModelAssembler grupoModelAssembler;
 	
+	@Override
 	@GetMapping
 	public List<GrupoModel> listar(@PathVariable Long usuarioId) {
 		Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
@@ -34,12 +38,14 @@ public class UsuarioGrupoController {
 		return grupoModelAssembler.toCollectionModel(usuario.getGrupos());
 	}
 	
+	@Override
 	@DeleteMapping("/{grupoId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
 		cadastroUsuario.desassociarGrupo(usuarioId, grupoId);
 	}
 	
+	@Override
 	@PutMapping("/{grupoId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {

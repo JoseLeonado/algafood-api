e2003d80d5a3af2f7d41a2aff0e7a58a3ae3278a
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-09-20 00:53:42 -0300

    Refatorando controladores para adicionar /v1 nas URIs

--- src/main/java/com/algaworks/algafood/api/v1/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/RestauranteController.java
@@ -33,7 +33,7 @@ import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
-@RequestMapping(path = "/restaurantes", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/restaurantes", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestauranteController implements RestauranteControllerOpenApi {
 
 	@Autowired
